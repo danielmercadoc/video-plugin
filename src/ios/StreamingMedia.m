738ddb5330b725869a7f62f86015fd0935fb74dd
Daniel <daniel@bpl.cl>
2019-08-07 23:44:24 -0400

    Cambios

--- src/ios/StreamingMedia.m
+++ src/ios/StreamingMedia.m
@@ -53,6 +53,8 @@ NSString * const DEFAULT_IMAGE_SCALE = @"center";
         initFullscreen = YES;
     }
     
+    requiresLinearPlayback = true;
+    
     if ([type isEqualToString:TYPE_AUDIO]) {
         videoType = TYPE_AUDIO;
         
